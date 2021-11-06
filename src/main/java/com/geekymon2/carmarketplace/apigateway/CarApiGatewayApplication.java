d66841c80f724bdb2133cb9d01dfcc61936a72ff
Geeky Mon2 <geekymon2@gmail.com>
2021-11-06 14:57:18 +1100

    updates properties

--- src/main/java/com/geekymon2/carmarketplace/apigateway/CarApiGatewayApplication.java
+++ src/main/java/com/geekymon2/carmarketplace/apigateway/CarApiGatewayApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CarApiGatewayApplication {
 
