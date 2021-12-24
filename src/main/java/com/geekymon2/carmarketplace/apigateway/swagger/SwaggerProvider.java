c500f1a4f575206f503ced8e1a291f1fa93b7961
Geeky Mon2 <geekymon2@gmail.com>
2021-12-24 18:00:03 +1100

    work in progress, aggregate swagger docs

---
+++ src/main/java/com/geekymon2/carmarketplace/apigateway/swagger/SwaggerProvider.java
@@ -0,0 +1,46 @@
+package com.geekymon2.carmarketplace.apigateway.swagger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Component
+@Primary
+public class SwaggerProvider implements SwaggerResourcesProvider {
+
+    public static final String API_URI = "/v2/api-docs";
+
+    public static final String EUREKA_SUB_PRIX = "CompositeDiscoveryClient_";
+
+    private final DiscoveryClientRouteDefinitionLocator routeLocator;
+
+    public SwaggerProvider(DiscoveryClientRouteDefinitionLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        routeLocator.getRouteDefinitions().subscribe(routeDefinition -> {
+            String resourceName = routeDefinition.getId().substring(EUREKA_SUB_PRIX.length());
+            String location = routeDefinition.getPredicates().get(0).getArgs().get("pattern").replace("/**", API_URI);
+            resources.add(swaggerResource(resourceName, location));
+        });
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;    
+    }
+}
