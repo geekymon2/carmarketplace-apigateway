2a5df5ad1a0318b2c5c66a06ff7796159faeba9f
Geeky Mon2 <geekymon2@gmail.com>
2021-12-30 17:16:41 +1100

    swagger working

--- src/main/java/com/geekymon2/carmarketplace/apigateway/config/AppConfig.java
+++ src/main/java/com/geekymon2/carmarketplace/apigateway/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.geekymon2.carmarketplace.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -24,5 +25,12 @@ public class AppConfig {
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes().build();
-    }   
+    }
+    
+    @Value("${spring.application.name}")
+    private String appName;    
+
+    public String getAppName() {
+        return appName;
+    }    
 }
--- src/main/java/com/geekymon2/carmarketplace/apigateway/swagger/SwaggerProvider.java
+++ src/main/java/com/geekymon2/carmarketplace/apigateway/swagger/SwaggerProvider.java
@@ -3,6 +3,9 @@ package com.geekymon2.carmarketplace.apigateway.swagger;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.geekymon2.carmarketplace.apigateway.config.AppConfig;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
@@ -13,34 +16,42 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 @Component
 @Primary
 public class SwaggerProvider implements SwaggerResourcesProvider {
-
+    
     public static final String API_URI = "/v2/api-docs";
-
-    public static final String EUREKA_SUB_PRIX = "CompositeDiscoveryClient_";
-
+    public static final String EUREKA_SUB_PRIX = "ReactiveCompositeDiscoveryClient_";
+    
     private final RouteDefinitionLocator routeLocator;
-
-    public SwaggerProvider(RouteDefinitionLocator routeLocator) {
+    private final AppConfig appConfig;
+    
+    public SwaggerProvider(RouteDefinitionLocator routeLocator, AppConfig appConfig) {
         this.routeLocator = routeLocator;
+        this.appConfig = appConfig;
     }
-
+    
     @Override
-
+    
     public List<SwaggerResource> get() {
+        final String GATEWAY_STRING = "/" + appConfig.getAppName();
         List<SwaggerResource> resources = new ArrayList<>();
         routeLocator.getRouteDefinitions().subscribe(routeDefinition -> {
-            String resourceName = routeDefinition.getId().substring(EUREKA_SUB_PRIX.length());
-            String location = routeDefinition.getPredicates().get(0).getArgs().get("pattern").replace("/**", API_URI);
-            resources.add(swaggerResource(resourceName, "/api" + location));
-        });
-        return resources;
+            if (routeDefinition.getId().contains(EUREKA_SUB_PRIX)) {
+                String resourceName = routeDefinition.getId().substring(EUREKA_SUB_PRIX.length());
+                String location = routeDefinition.getPredicates().get(0).getArgs().get("pattern").replace("/**", API_URI);
+                if (location.contains(GATEWAY_STRING)) {
+                    location = location.replace(GATEWAY_STRING, StringUtils.EMPTY);
+                }
+                resources.add(swaggerResource(resourceName, location));
+            }});
+            
+            return resources;
+        }
+        
+        private SwaggerResource swaggerResource(String name, String location) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion("2.0");
+            return swaggerResource;    
+        }
     }
-
-    private SwaggerResource swaggerResource(String name, String location) {
-        SwaggerResource swaggerResource = new SwaggerResource();
-        swaggerResource.setName(name);
-        swaggerResource.setLocation(location);
-        swaggerResource.setSwaggerVersion("2.0");
-        return swaggerResource;    
-    }
-}
+    
