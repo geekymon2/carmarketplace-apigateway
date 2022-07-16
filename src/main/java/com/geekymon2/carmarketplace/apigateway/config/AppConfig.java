5b6ed0d64fb51e91beff2da0e1d6203d8ed65d94
Geeky Mon2 <geekymon2@gmail.com>
2022-07-16 18:04:09 +1000

    openapi, version upgrade

--- src/main/java/com/geekymon2/carmarketplace/apigateway/config/AppConfig.java
+++ src/main/java/com/geekymon2/carmarketplace/apigateway/config/AppConfig.java
@@ -1,21 +1,45 @@
 package com.geekymon2.carmarketplace.apigateway.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
+@Slf4j
 public class AppConfig {
+    private static final String API_URI = "/v3/api-docs";
+
+    private final RouteDefinitionLocator locator;
+
+    public AppConfig(RouteDefinitionLocator locator) {
+        this.locator = locator;
+    }
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+
+
+        locator.getRouteDefinitions().subscribe(routeDefinition -> {
+            log.info("Discovered route definition: {}", routeDefinition.getId());
+            String resourceName = routeDefinition.getId();
+            String location = routeDefinition.getPredicates().get(0).getArgs().get("_genkey_0").replace("/**", API_URI);
+            log.info("Adding swagger resource: {} with location {}", resourceName, location);
+
+            GroupedOpenApi api = GroupedOpenApi.builder()
+                    .group(resourceName)
+                    .pathsToMatch("/" + location + "/**")
+                    .build();
+
+            groups.add(api);
+        });
+
+        return groups;
     }
 }
